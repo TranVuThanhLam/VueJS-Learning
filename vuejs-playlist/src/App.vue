9cf55020af1b1613dfa29623ee28ee387f6a4960
rs-lamtvt <lamtvt@runsystem.net>
2026-03-03 17:25:34 +0700

    basic

---
+++ vuejs-playlist/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
+</style>
